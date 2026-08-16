91fafb79b1721c6c2c7c5bff6015f15dcc8c2e53
Kong Trong <kongtrong21@gmail.com>
2026-08-15 23:25:56 -0600

    look in shopHandler

--- shopHandler.js
+++ shopHandler.js
@@ -1,6 +1,5 @@
 import {newRect} from "./Utility.js";
 
-
 //you will need to make dialogueClass modular for this to work since the keeper's dialogue will want to use its system
 export let shopHandler = {
     menuStack: [{name:"main",curIndex:0,maxIndex:3}],
@@ -13,7 +12,7 @@ export let shopHandler = {
     draw() {
         newRect("a", 100, 100, 200, 200, "rgb(192,82,200)").draw();
 
-
+        newRect("id", 800, 600, 700, 400, "rgb(255,255,255)").draw();
 
 
 
