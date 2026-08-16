ef4495b2bfb4ec9c5f058d99665076e99822d2b1
codeInvaild <kongtrong21@gmail.com>
2026-08-15 23:17:33 -0600

    testing saves, look in deathHandler to see changes

--- deathHandler.js
+++ deathHandler.js
@@ -1,2 +1,15 @@
 //this needs to handle deaths and how to revert back to save states (saveHandler)
 //this only handles battle deaths for now
+
+
+
+
+export const deathHandler={
+    update(dt) {
+        
+    },
+    draw() {
+        
+        //draw 
+    }
+}
